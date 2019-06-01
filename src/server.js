378459b27366bbac77344a3b4542b1aacd2869d6
Ankit Singh <ankit.zin279@gmail.com>
2019-06-01 16:13:57 +0530

    Checking

--- src/server.js
+++ src/server.js
@@ -24,9 +24,7 @@ io.on('connection', (socket) => {
         if (error) {
             return callback(error)
         }
-
         socket.join(user.room)
-
         socket.emit('message', generateMessage('Admin', 'Welcome!'))
         socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
         io.to(user.room).emit('roomData', {
